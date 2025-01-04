3ed9d3d30581eaef2bc3aea11795061d244ade87
Tim Rogers <me@timrogers.co.uk>
2025-01-04 21:21:01 +0000

    Autocorrect ESLint issues

--- scripts/generate_airports_json.js
+++ scripts/generate_airports_json.js
@@ -1,30 +1,30 @@
 #!/usr/bin/node
 
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-import path from 'path'
+import path from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs/promises'
-import { Duffel } from '@duffel/api'
+import fs from 'fs/promises';
+import { Duffel } from '@duffel/api';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const OUTPUT_PATH = path.join(__dirname, '../', 'data', 'airports.json');
 
 const duffel = new Duffel({
-  token: process.env.DUFFEL_ACCESS_TOKEN
+  token: process.env.DUFFEL_ACCESS_TOKEN,
 });
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const fetchAndWriteAirports = async () => {
   let airports = [];
 
   for await (const airportResponse of duffel.airports.listWithGenerator()) {
     console.log(`Loaded airport ${airportResponse.data.iata_code} ✅`);
-    
+
     // `airportResponse` can contain properties that aren't defined in the
     // `Airline` type. If this is the case, they'll still be included in our
     // list and written to the file.
@@ -38,7 +38,7 @@ const fetchAndWriteAirports = async () => {
   }
 
   await fs.writeFile(OUTPUT_PATH, JSON.stringify(airports));
-}
+};
 
 fetchAndWriteAirports()
   .then(() => console.log(`Wrote airports data to ${OUTPUT_PATH}`))
