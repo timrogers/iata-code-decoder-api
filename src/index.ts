1c03d5a8eb0353f125bf6e3f6ba0f79f86fa4fd0
Tim Rogers <me@timrogers.co.uk>
2026-01-20 09:17:05 +0100

    Fix PORT environment variable

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = Number.parseInt(process.env.PORT ?? '4000', 10);
 
 app.listen(PORT, (): void =>
   console.log(`✈️  IATA Code Decoder API running on port ${PORT}`),
